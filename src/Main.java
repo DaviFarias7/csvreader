52ed4087d6c9d32d9d858ae4aef42c3b1f200d84
DaviFarias7 <davic.farias7@gmail.com>
2023-10-22 11:23:58 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path = "c:\\temp\\in.txt";
+
+        List<Product> list = new ArrayList<Product>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path)))  {
+
+            String line = br.readLine();
+            line = br.readLine();
+            while (line != null) {
+                String[] vect = line.split(",");
+                String name = vect[0];
+                Double price = Double.parseDouble(vect[1]);
+                Integer qte = Integer.parseInt(vect[2]);
+
+                Product prod = new Product(name, price, qte);
+                list.add(prod);
+
+                line = br.readLine();
+            }
+            System.out.println("PRODUCTS: ");
+             for (Product p : list) {
+                 System.out.println(p);
+             }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+     }
+}
